f62cc771be5a787f51bc6f633cf307dde7ff4c54
Thibault-Monnier <thibaultmonni@gmail.com>
2025-07-16 23:53:05 +0200

    Handle window and basic drawing + basic camera movement

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,117 @@
-#include <iostream>
+#include "raylib.h"
+#include "raymath.h"
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in
-// the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how
-    // CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+void runMenu() {
+    while (!(WindowShouldClose() || IsKeyPressed(KEY_ENTER))) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon
-        // src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+        DrawText("Welcome to the Minecraft Clone!", 190, 200, 30, ORANGE);
+        DrawText("Press ENTER to launch the game", 220, 250, 20, DARKGRAY);
+
+        EndDrawing();
     }
+}
 
-    return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you
-    // can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
+void drawCursor() {
+    int screenWidth = GetScreenWidth();
+    int screenHeight = GetScreenHeight();
+    Vector2 center = {static_cast<float>(screenWidth) / 2.0f,
+                      static_cast<float>(screenHeight) / 2.0f};
+
+    DrawCircleLinesV(center, 7.0f, BLACK);  // Draw a circle around the cursor
+    DrawCircleLinesV(center, 1.0f, BLACK);  // Draw a dot in the center
+}
+
+void drawGame(const Camera& camera) {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+
+    BeginMode3D(camera);
+
+    Vector3 cubePosition = {0.5f, 0.5f, 0.5f};
+    DrawCube(cubePosition, 1.0f, 1.0f, 1.0f, BROWN);
+    DrawCubeWires(cubePosition, 1.0f, 1.0f, 1.0f, DARKGRAY);
+    DrawGrid(1000, 1.0f);  // Draw a grid for reference
+
+    EndMode3D();
+
+    drawCursor();
+
+    EndDrawing();
+}
+
+void runGame() {
+    DisableCursor();
+
+    Camera camera;
+    camera.position = {-4.0f, 4.0f, 0.0f};
+    camera.target = {0.0f, 0.0f, 0.0f};
+    camera.up = {0.0f, 1.0f, 0.0f};
+    camera.fovy = 45.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+
+    const float cameraMovementSpeed = 2.0f;   // Speed of camera movement
+    const float cameraSensitivity = 0.0025f;  // Sensitivity for mouse movement
+
+    float cameraYaw = 0.0f;    // Yaw angle for camera rotation
+    float cameraPitch = 0.0f;  // Pitch angle for camera rotation
+
+    while (!WindowShouldClose()) {
+        Vector2 mouseDelta = GetMouseDelta();
+        cameraYaw += mouseDelta.x * cameraSensitivity;
+        cameraPitch -= mouseDelta.y * cameraSensitivity;
+        cameraPitch = Clamp(cameraPitch, -89.0f / 180.0f * M_PI,
+                            89.0f / 180.0f * M_PI);  // Limit pitch to avoid flipping
+
+        Vector3 direction = {cosf(cameraYaw) * cosf(cameraPitch), sinf(cameraPitch),
+                             sinf(cameraYaw) * cosf(cameraPitch)};
+
+        camera.target = Vector3Add(camera.position, direction);
+
+        float moveForward = 0.0f;
+        float moveSide = 0.0f;
+        float moveUp = 0.0f;
+
+        if (IsKeyDown(KEY_W))
+            moveForward = 1.0f;
+        else if (IsKeyDown(KEY_S))
+            moveForward = -1.0f;
+
+        if (IsKeyDown(KEY_A))
+            moveSide = 1.0f;
+        else if (IsKeyDown(KEY_D))
+            moveSide = -1.0f;
+
+        if (IsKeyDown(KEY_SPACE))
+            moveUp = 1.0f;
+        else if (IsKeyDown(KEY_LEFT_SHIFT))
+            moveUp = -1.0f;
+
+        const float deltaTime = GetFrameTime();
+
+        Vector2 forward2D = {cosf(cameraYaw), sinf(cameraYaw)};
+        Vector2 right2D = {forward2D.y, -forward2D.x};
+
+        camera.position.x += (forward2D.x * moveForward + right2D.x * moveSide) *
+                             cameraMovementSpeed * deltaTime;
+        camera.position.z += (forward2D.y * moveForward + right2D.y * moveSide) *
+                             cameraMovementSpeed * deltaTime;
+
+        camera.position.y += moveUp * cameraMovementSpeed * deltaTime;
+
+        drawGame(camera);
+    }
+}
+
+int main() {
+    InitWindow(0, 0, "Minecraft Clone");
+    ToggleFullscreen();
+
+    SetTargetFPS(60);  // Set the game to run at 60 frames per second
+
+    runMenu();
+    runGame();
+
+    CloseWindow();  // Close the window and OpenGL context
 }
